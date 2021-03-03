976563172b2c9018e7778a9bbafaa4a6b311e0fb
kyoya0819 <kyoya0819@gmail.com>
2021-03-03 22:48:53 +0900

    表示内容の変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,63 @@
 import React, { Component } from 'react';
 
-import CreateCard from './../components/index/create-card';
-import projects from './../data/projects.json';
+import skills from './../data/skills.json';
 
 import './index.scss';
 
 import moment from 'moment';
 import 'moment-timezone';
+import {useTranslation} from "react-i18next";
 
-class App extends Component {
+const year = () => {
+    moment.tz.setDefault('Asia/Tokyo');
 
-    year = () => {
-        moment.tz.setDefault('Asia/Tokyo');
+    const now_year = moment().year();
+    const birth_year = 2003;
 
-        const now_year = moment().year();
-        const birth_year = 2003;
+    const now_date = Number(moment().month() + 1 + '' + moment().date());
+    const birth_date = 819;
 
-        const now_date = Number(moment().month() + 1 + '' + moment().date());
-        const birth_date = 819;
-
-        if (now_date >= birth_date)
-            return now_year - birth_year;
-        else
-            return now_year - birth_year - 1;
-    }
-
-    render() {
-        document.title = "kyoya0819 | TOP";
-        return (
-            <div className="inner">
-                <section>
-                    <h3>ABOUT</h3>
-                    <div className="left">
-                        <p>日本に住む、{this.year()}歳のシロクマ。</p>
-                        <p>PHP大好きの生粋のPHPer。</p>
-                        <p>普段はバックエンドが主、デザイン・ロゴ作成もしばしば。</p>
-                    </div>
-                </section>
-                <section>
-                    <h3>CREATE</h3>
-                    <ul id="creates">
-                        { projects.map((project, i) => <CreateCard key={i} idNum={i+1} data={project} />) }
-                    </ul>
-                </section>
-            </div>
-        );
-    }
+    if (now_date >= birth_date)
+        return now_year - birth_year;
+    else
+        return now_year - birth_year - 1;
 }
 
-export default App;
+const Index = () => {
+    const { t } = useTranslation();
+
+    document.title = "kyoya0819 | TOP";
+
+    return (
+        <div className="inner">
+            <section>
+                <h3>ABOUT</h3>
+                <div className="left">
+                    <p>{ t('日本に住む、{{year}}歳のシロクマ。', { year: year() }) }</p>
+                    <p>{ t('PHP大好きの生粋のPHPer。') }</p>
+                    <p>{ t('普段はバックエンドが主、デザイン・ロゴ作成もしばしば。') }</p>
+                </div>
+            </section>
+            <section>
+                <h3>SKILLS</h3>
+                <ul id="skills">
+                    { skills.map((name, i) => <li key={ i }><span>{ name }</span></li>) }
+                </ul>
+            </section>
+            <section>
+                <h3>LINKS</h3>
+                <ul id="links">
+                    <li><a href="https://twitter.com/kyoya0819" target="_blank" rel="noreferrer">Twitter</a></li>
+                    <li><a href="https://www.facebook.com/profile.php?id=100054776536464" target="_blank" rel="noreferrer">Facebook</a></li>
+                    <li><a href="https://github.com/kyoya0819" target="_blank" rel="noreferrer">GitHub</a></li>
+                    <li><a href="https://www.resume.id/kyoya0819" target="_blank" rel="noreferrer">RESUME</a></li>
+                    <li><a href="https://teratail.com/users/kyoya0819" target="_blank" rel="noreferrer">teratail</a></li>
+                    <li><a href="https://qiita.com/kyoya0819" target="_blank" rel="noreferrer">Qiita</a></li>
+                    <li><a href="https://pw.kyoya0819.com/" target="_blank" rel="noreferrer">PasswordMarker</a></li>
+                </ul>
+            </section>
+        </div>
+    );
+}
+
+export default Index;
