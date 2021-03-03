cfe1c05be7b6647b91219f96e2355e14ef047f7c
kyoya0819 <kyoya0819@gmail.com>
2021-03-03 22:48:42 +0900

    改名

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+import scss from './Error404.module.scss';
+
 class App extends Component {
     render() {
         document.title = "kyoya0819 | NotFound";
         return (
-            <div className="inner">
-                存在しません
+            <div className={`inner ${ scss.center }`}>
+                存在しません。<br />
+                Not Found.
             </div>
         );
     }
