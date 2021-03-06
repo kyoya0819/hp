9c87188118b72fa7458fd2c46d07ecbfd8eac98f
kyoya0819 <kyoya0819@gmail.com>
2021-03-06 21:47:35 +0900

    スタイルの共通化

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import 'moment-timezone';
 
+import scss from './index.module.scss';
+
 import Top from './top/top';
 import ProgrammingSkills from './skills/programming';
 import OtherSkills from './skills/other';
@@ -11,7 +13,7 @@ const Index = () => {
     document.title = "kyoya0819 | TOP";
 
     return (
-        <div className="inner">
+        <div className={`inner ${ scss.index }`}>
             <Top />
 
             <ProgrammingSkills />
