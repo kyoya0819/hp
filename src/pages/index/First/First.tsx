e84c74810e8159a04f6c54095deeaa2f84265ce2
kyoya0819 <kyoya0819@gmail.com>
2021-05-04 00:14:21 +0900

    画像読み込みの高速化

--- src/pages/index/First/First.tsx
+++ src/pages/index/First/First.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import scss from './First.module.scss';
 
 import background from './../../../images/background.webp';
+import background_min from './../../../images/background-min.webp';
 
 const First = () => {
 
@@ -12,9 +13,15 @@ const First = () => {
         first_tag.style.height = window.innerHeight + 'px';
     });
 
+    useEffect(() => {
+
+        let background_image = document.getElementById("sky") as HTMLElement;
+        background_image.setAttribute('src', background);
+    }, []);
+
     return (
         <div className={ scss.first }>
-            <img src={ background } alt="sky" />
+            <img src={ background_min } id="sky" alt="sky" />
             <div className="inner">
                 <h1>kyoya0819</h1>
             </div>
