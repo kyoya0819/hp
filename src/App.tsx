cf54d8d32371416ed9cda2d82679151652cc5c4b
kyoya0819 <kyoya0819@gmail.com>
2021-05-04 21:44:14 +0900

    記述箇所の移動

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import route from './Route';
 
 import Index from './pages/index/Index';
 import NotFound from './pages/Error404';
 
-import Footer from './components/Footer/Footer';
-
 const App = () => {
 
     return (
-        <Router>
-            <main >
-                <Switch>
-                    <Route exact path={route('index')} component={Index} />
-                    <Route component={NotFound} />
-                </Switch>
-            </main>
-            <Footer />
-        </Router>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path={ route('index') } component={ Index } />
+                <Route component={ NotFound } />
+            </Switch>
+        </BrowserRouter>
     );
 }
 
--- src/pages/index/Index.tsx
+++ src/pages/index/Index.tsx
@@ -7,18 +7,23 @@ import Skills from './Skills/Skills';
 import Links from './Links/Links';
 import Contact from './Contact/Contact';
 
+import Footer from './../../components/Footer/Footer';
+
 const Index = () => {
 
     document.title = "kyoya0819 | TOP";
 
     return (
         <Fragment>
-            <First />
-            <About />
-            <Histories />
-            <Skills />
-            <Links />
-            <Contact />
+            <main>
+                <First />
+                <About />
+                <Histories />
+                <Skills />
+                <Links />
+                <Contact />
+            </main>
+            <Footer />
         </Fragment>
     );
 }
