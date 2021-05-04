f3a611433a26cf3409b2a63b9bce703e3e2858bd
kyoya0819 <kyoya0819@gmail.com>
2021-05-04 16:44:09 +0900

    アニメーションの追加

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import scss from './Title.module.scss';
 
@@ -8,11 +8,28 @@ interface TitleInterface {
 
 const Title = (props: TitleInterface) => {
 
+    useEffect(() => {
+        document.addEventListener('scroll', onScroll);
+        return () => document.removeEventListener('scroll', onScroll);
+    });
+
     return (
         <div className={ scss.title }>
             <h2>{ props.title }</h2>
         </div>
     );
 };
+
+const onScroll = () => {
+
+    let elements = document.getElementsByClassName( scss.title );
+    Object.keys(elements).forEach((key) => {
+        let element = elements[Number(key)];
+        let top = element.getBoundingClientRect().top;
+
+        if (window.pageYOffset + window.innerHeight > window.pageYOffset + top)
+            element.classList.add(scss.view);
+    });
+}
 
 export default Title;
