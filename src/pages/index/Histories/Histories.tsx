e51a2ea6237d2c924d71f094e39918480e586a5c
kyoya0819 <kyoya0819@gmail.com>
2021-05-21 17:25:44 +0900

    Update Histories.tsx

--- src/pages/index/Histories/Histories.tsx
+++ src/pages/index/Histories/Histories.tsx
@@ -11,10 +11,10 @@ const Histories = () => {
             <div className="inner">
                 <Title title="経歴 / Histories" />
                 <ul className={ scss.list }>
-                    <li>2003年 - 神奈川県生まれ</li>
-                    <li>2010年 - 千葉県の公立小学校に入学</li>
+                    <li>2003年 - 生まれる</li>
+                    <li>2010年 - 公立小学校に入学</li>
                     <li>2013年 - Yahoo! JAPANを作ることを夢見て、プログラミングを始める</li>
-                    <li>2016年 - 千葉県の公立中学校に入学</li>
+                    <li>2016年 - 公立中学校に入学</li>
                     <li>2017年 - 個人事業主2社の元でWordPressサイトの改修, 保守, SEO対策等を経験。</li>
                     <li>2019年 - 日本大学の附属高校に入学</li>
                     <li>2020年 - 高校1年生までの集大成としてLaravelを使って、良質な質問ができるようなプログラミング専用のQ&Aサイトを立ち上げ</li>
