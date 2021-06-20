9c19d70a729c58ae91be4c015429b014559d997b
kyoya0819 <kyoya0819@gmail.com>
2021-06-20 14:04:40 +0900

    自動化

--- src/pages/index/Histories/Histories.tsx
+++ src/pages/index/Histories/Histories.tsx
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { FC } from 'react';
 
 import scss from './Histories.module.scss';
 
 import Title from 'components/Title/Title';
+import histories_json from "data/histories.json";
 
-const Histories = () => {
+const Histories: FC = () => {
+
+    const histories_object: {[key: string]: string} = histories_json;
 
     return (
         <div className={ scss.history }>
             <div className="inner">
                 <Title title="経歴 / Histories" />
-                <ul className={ scss.list }>
-                    <li>2003年 - 生まれる</li>
-                    <li>2010年 - 公立小学校に入学</li>
-                    <li>2013年 - Yahoo! JAPANを作ることを夢見て、プログラミングを始める</li>
-                    <li>2016年 - 公立中学校に入学</li>
-                    <li>2017年 - 個人事業主2社の元でWordPressサイトの改修, 保守, SEO対策等を経験。</li>
-                    <li>2019年 - 日本大学の附属高校に入学</li>
-                    <li>2020年 - 高校1年生までの集大成としてLaravelを使って、良質な質問ができるようなプログラミング専用のQ&Aサイトを立ち上げ</li>
-                    <li>2021年 - 株式会社FARMER'Sで、HP作成, Shopifyのテーマ作成, 独自のECプラットフォームの開発を経験</li>
-                </ul>
+                <ul className={ scss.list }>{
+                    Object.keys(histories_object).map((key) => {
+                        return <History year={key} content={histories_object[key]} /> }
+                    )
+                }</ul>
             </div>
         </div>
     );
+};
+
+
+interface HistoryInterface {
+    year: string,
+    content: string
+}
+
+const History: FC<HistoryInterface> = ({ year, content }) => {
+    return <li>{year}年 - {content}</li>;
 };
 
 export default Histories;
