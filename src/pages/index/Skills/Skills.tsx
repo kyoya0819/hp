189fc87fae4184b94a2ecda3579f73c430ee563b
kyoya0819 <kyoya0819@gmail.com>
2021-08-11 20:01:11 +0900

    記法を他に準拠

--- src/pages/index/Skills/Skills.tsx
+++ src/pages/index/Skills/Skills.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement } from "react";
+import React, { FC } from "react";
 
 import scss from "./Skills.module.scss";
 
@@ -7,18 +7,16 @@ import Title from "components/Title/Title";
 import json from "data/skills.json";
 
 const Skills: FC = () => {
-    
-    const array_skills: ReactElement[] = [];
-    Object.keys(json).forEach((key: string) => {
-        const skill_name = json[Number(key)];
-        array_skills.push(<li>{ skill_name }</li>);
-    });
 
     return (
         <section className={ scss.skills }>
             <div className="inner">
                 <Title>スキル / Skills</Title>
-                <ul>{ array_skills.map((content) => content) }</ul>
+                <ul>{
+                    Object.keys(json).map((key: string) => {
+                        return <li key={ key }>{ json[Number(key)] }</li>;
+                    })
+                }</ul>
             </div>
         </section>
     );
