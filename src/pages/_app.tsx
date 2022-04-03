765acfde5dc6e9b970bfd5de9494510e9d5cc127
kyoya0819 <kyoya0819@gmail.com>
2022-04-04 01:26:14 +0900

    Apply OGP 👀

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         <>
             <Head>
                 <title />
+                <meta property="og:url" content="https://kyoya0819.com" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="kyoya0819" />
+                <meta property="og:site_name" content="kyoya0819 | ホームページ" />
+                <meta property="og:description" content="こたつで一日中生活するシロクマエンジニア。" />
+                <meta property="og:image" content="https://kyoya0819.com/ogp.png" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="description" content="こたつで一日中生活するシロクマエンジニア。" />
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
                 <link rel="manifest" href="/manifest.json" />
