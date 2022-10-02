087a9fb0a91e2d2ddd9425b25ebf7a8e3b808a65
kyoya0819 <kyoya0819@gmail.com>
2022-10-03 02:57:29 +0900

    wip

--- src/sections/index/Hero/index.tsx
+++ src/sections/index/Hero/index.tsx
@@ -8,19 +8,17 @@ import scss from "./index.module.scss";
 const Hero: FC = () => {
 
     return (
-        <>
-            <section>
-                <div className={ scss.hero }>
-                    <div className="inner">
-                        <h1>
-                            <img src={ kyoya0819.src } width={ kyoya0819.width } height={ kyoya0819.height } alt="kyoya0819" />
-                            <span />
-                            <img src={ miyako_nari.src } width={ miyako_nari.width } height={ miyako_nari.height } alt="Miyako Nari" />
-                        </h1>
-                    </div>
+        <section>
+            <div className={ scss.hero }>
+                <div className="inner">
+                    <h1>
+                        <img src={ kyoya0819.src } width={ kyoya0819.width } height={ kyoya0819.height } alt="kyoya0819" />
+                        <span />
+                        <img src={ miyako_nari.src } width={ miyako_nari.width } height={ miyako_nari.height } alt="Miyako Nari" />
+                    </h1>
                 </div>
-            </section>
-        </>
+            </div>
+        </section>
     );
 };
 export default Hero;
